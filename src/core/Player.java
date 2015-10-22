ee63ea98bf81e4b3d1f0e2252a679efe92861aec
isitso <ryan033111@gmail.com>
2015-10-22 09:43:21 -0700

    added Player class to hold username, account_id, and character

---
+++ src/core/Player.java
@@ -0,0 +1,27 @@
+package core;
+
+public class Player {
+	String username;
+	Character character;
+	int id;	// account id
+	
+	// Getters and Setters
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Character getCharacter() {
+		return character;
+	}
+	public void setCharacter(Character character) {
+		this.character = character;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
